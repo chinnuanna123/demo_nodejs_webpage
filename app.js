71aef02eb68ef98261e4ff110c0adb0f88bb3bcf
kurianchinnuanna <151050356+chinnuanna123@users.noreply.github.com>
2024-10-25 19:08:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express();
 const path = require('path');
 
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
   
 });
 
 
-app.get('/about', (req,res) => {
-    res.sendFile(path.join(__dirname+'/about.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
    
   });
 
