4c75058a9e075f93b2fd9638dc2177c4b109e760
kurianchinnuanna <151050356+chinnuanna123@users.noreply.github.com>
2024-10-25 19:18:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
